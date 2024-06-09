c0f2308da91c8f367558079690d5fbc7d08451be
Erfan Mazruei <erfanmzrx@gmail.com>
2024-06-09 17:53:23 +0330

    second commit

--- bankp2.cpp
+++ bankp2.cpp
@@ -5,8 +5,69 @@
 #include <set>
 #include <random>
 #include <ctime>
+#include <string>
+
 
 using namespace std;
+
+template <typename T>
+class node {
+public :
+	T d ;
+	node * n ;
+};
+
+template <typename T>
+class linklist
+{
+    private:
+	node<T> * h ;
+	int size ;
+    public:
+    linklist()
+    {
+        h=nullptr;
+        size=0;
+    }
+    ~linklist()
+    {
+        node<T> * tmp = this->h ;
+
+        while ( tmp != 0 )
+        {
+            node<T>* tmp2 = tmp->n ;
+            delete tmp ;
+            tmp = tmp2;
+        }
+        this->h = nullptr ;
+        this->size = 0 ;
+    }
+    void push_front(T num)
+    {
+        node<T> * tmp = new node<T> ;
+	    tmp -> d = num ;
+	    tmp -> n = this->h ;
+	    this->h = tmp ;
+	    this->size ++ ;
+    }
+    void print ()
+    {
+        cout << "printing linklist of size " << size << endl ;
+	    node<T> * tmp = this->h ;
+	    while ( tmp != 0 )
+        {
+		    cout << (*tmp).d << endl ;
+		    tmp = tmp -> n;
+	    }
+    }
+
+    T find( T x)
+    {
+        node<T> * tmp = this->h ;  
+    }
+
+
+};
 
 class bankaccount
 {
@@ -24,7 +85,7 @@ class bankaccount
     string secondstaticpass;
     string seconddynamicpass;
     void cvv2Maker()
-    {   
+    {
         cvv2="100"+counterforcvv2;
         counterforcvv2++;
     }
@@ -39,6 +100,7 @@ int bankaccount::counterforcvv2=1;
 int bankaccount::counterforshaba=1;
 class user
 {
+
     public:
     string firstname;
     string lastname;
@@ -49,31 +111,56 @@ class user
     bankaccount accounts[5];
 
 
+
+
+
 };
 
 set<int> generatedNumbers;
 
-int generateUniqueRandomNumber() 
+int generateUniqueRandomNumber()
 {
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(100000, 999999);
     int randomNumber;
-    do 
+    do
     {
         randomNumber = dist(gen);
-    } 
+    }
     while (generatedNumbers.find(randomNumber) != generatedNumbers.end());
     generatedNumbers.insert(randomNumber);
     return randomNumber;
 }
 
+linklist<user> users;
+
 
 void sign_up()
 {
-    
+    user newuser;
+    cout << "Enter your name:" << endl;
+    cin >> newuser.firstname;
+    cout << "Enter your last name:" << endl;
+    cin >> newuser.lastname;
+    cout << "Enter your id number:" << endl;
+    cin >> newuser.idnumber;
+    cout << "Enter your age:" << endl;
+    cin >> newuser.age;
+    cout << "Enter your username:" << endl;
+    cin >> newuser.username;
+    cout << "Enter your password:" << endl;
+    cin >> newuser.password;
+    users.push_front(newuser);
+    cout << "signed up successfully" << endl;
 }
+void log_in ()
+{
+    string tmpusername;
+    cin >> tmpusername;
+    
 
+}
 
 int main()
 {
